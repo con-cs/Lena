aa6ddc273db990b70d0ade66a66602422a3343b3
Christian Schulte <schulte@connectiv.de>
2022-02-14 14:52:58 +0100

    fixed qa repo url

--- WebsiteCode/qa/QA-RepoString.js
+++ WebsiteCode/qa/QA-RepoString.js
@@ -1,8 +1,10 @@
-const iframeURL = "https://htmlpreview.github.io/?https://github.com/con-cs/Lena/blob/main/index.html";
+const repoUrl = "https://github.com/con-cs/Lena/blob/main/WebsiteCode/index.html";
 
 function initIframe(){
     let iframe = document.getElementsByTagName('iframe');
     for (let i = 0; i < iframe.length; i++){
+        let iframeURL = "https://htmlpreview.github.io/?" + repoUrl;
+
         iframe[i].src = iframeURL;
     }
 }
